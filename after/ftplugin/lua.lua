8d29a52e4b0a9aeb108905bd73f337d0a6646c91
Renan Rocha Darós <renanrdaros@gmail.com>
2022-12-09 16:15:20 -0300

    Set indent size for lua

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+local opt = vim.opt
+
+opt.tabstop = 2           -- number of spaces that a <Tab> in the file counts for
+opt.shiftwidth = 2        -- number of spaces to use for each step of (auto)indent
