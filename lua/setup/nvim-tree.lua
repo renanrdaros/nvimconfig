393a39f42343700e613042304a523dca6b103dcd
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2023-05-18 22:00:12 -0300

    Update nvim-tree view settings

--- lua/setup/nvim-tree.lua
+++ lua/setup/nvim-tree.lua
@@ -1,5 +1,8 @@
 require("nvim-tree").setup({
   view = {
+    adaptive_size = true,
+    number = true,
+    relativenumber = true,
     mappings = {
       list = {
         { key = "g?", action = "" },
