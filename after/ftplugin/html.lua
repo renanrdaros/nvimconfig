27c4fbbb6a5c68840fcc3e66d3a730a55a72b402
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2023-10-26 17:50:34 -0300

    Make ftplugin settings local to buffer

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,2 +1,2 @@
-vim.opt.tabstop = 2           -- number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 2        -- number of spaces to use for each step of (auto)indent
+vim.opt_local.tabstop = 2           -- number of spaces that a <Tab> in the file counts for
+vim.opt_local.shiftwidth = 2        -- number of spaces to use for each step of (auto)indent
